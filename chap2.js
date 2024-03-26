1951d12d0e5fca9e0a53704f98cd0f9777f07cde
Moosadeveloper <janmoosa77@gmail.com>
2024-03-26 20:17:57 +0500

    first commit

---
+++ chap2.js
@@ -0,0 +1,19 @@
+var a="myName"
+
+
+
+var name ="hello";
+name ="moosa"
+alert(name);
+
+a="Pakistan"
+alert(a);
+a="India"
+alert(a);
+a="China"
+alert(a);
+
+
+var bestMan = "Charlie";
+bestMan ="only";
+alert(bestMan);
